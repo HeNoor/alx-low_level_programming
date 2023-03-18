a07e74ea7b532e164b78b27ff3fa6045b1d06841
HeNoor <hechanenourelhouda@gmail.com>
2023-03-18 13:32:13 +0000

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,17 +8,20 @@ int main(void)
 {
 	int n = 48;
 
-	while (n < 57)
+	while (n <= 57)
 	{
 		putchar(n);
-		putchar(44);
-		putchar(32);
-		n++;
 		if (n == 57)
 		{
-			putchar(n);
 			continue;
 		}
+		else
+		{
+			putchar(44);
+			putchar(32);
+			n++;
+		}
 	}
+	putchar('\n');
 	return (0);
 }
