bf2cd6bc4336beed3eaa8c7f78607099bbe588c7
HeNoor <hechanenourelhouda@gmail.com>
2023-03-18 13:03:55 +0000

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n = 48;
+
+	while (n <= 57)
+	{
+		putchar(n);
+		n++;
+	}
+	putchar(44);
+	putchar(32);
+	putchar('\n');
+	return (0);
+}
