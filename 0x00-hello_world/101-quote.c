cff8eaf055138836fbbef7b3f94e07feb88704d3
HeNoor <hechanenourelhouda@gmail.com>
2023-03-20 19:17:40 +0000

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,10 +2,12 @@
 #include <unistd.h>
 /**
  * main - A c program that prints a line to the standar error
- * Return 1 (Success)
+ * Return 1 (not success)
  */
 int main(void)
 {
-	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
-return (1);
+	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(1,quo,59);
+	return (1);
 }
