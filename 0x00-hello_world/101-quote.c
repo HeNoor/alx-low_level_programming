75306e37b3065c4c8548f5ff4c4c46417c91a98b
HeNoor <hechanenourelhouda@gmail.com>
2023-03-20 19:23:47 +0000

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - A c program that prints a line to the standar error
+ * main - Entry point
+ *
+ * Description: print a quote using write function
+ * 	write(int fd,const void *buf,size_t count)
+ *
  * Return 1 (not success)
- */
+*/
 int main(void)
 {
 	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
