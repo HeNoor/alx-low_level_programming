a730523e1a32bff8b0147d1a2c1143b6b0c5566d
HeNoor <hechanenourelhouda@gmail.com>
2023-03-22 09:12:23 +0000

    8-24_hours.c

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * jack_bauer - Entry point
+ *
+ * Description: prints every minute of the day of Jack Bauer
+ *
+ * Return: Always 0 (Success)
+ */
+
+void jack_bauer(void)
+{
+	int a;
+	int b;
+
+	for (a = 0; a <= 23; a++)
+	{
+		for (b = 0; b <= 59; b++)
+		{
+			_putchar((a / 10) + 48);
+			_putchar((a % 10) + 48);
+			_putchar(':');
+			_putchar((b / 10) + 48);
+			_putchar((b % 10) + 48);
+			_putchar('\n');
+		}
+	}
+}
