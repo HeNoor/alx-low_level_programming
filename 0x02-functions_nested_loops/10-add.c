8003eb621d54e621b66cb0aa4b3cb4a4a5c31dd4
HeNoor <hechanenourelhouda@gmail.com>
2023-03-22 17:01:07 +0000

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * add - Entry point
+ *
+ * Description: adds two integers and returns the result
+ *
+ * @n: checks 1st input of function
+ *
+ * @m: checks 2sd input of function
+ *
+ * Return: Always 0 (Success)
+ */
+
+int add(int n, int m)
+{
+	int sum;
+
+	sum = n + m;
+	return (sum);
+}
