3ec80e9928ae92dce1d653a61b686eca379abe21
HeNoor <hechanenourelhouda@gmail.com>
2023-03-22 08:09:34 +0000

    5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_sign - Entry point
+ *
+ * Description: check if a number is positive, negative or nul
+ *
+ * @n: checks input of function
+ *
+ * Return: returns 1 if positive, 0 if zero, -1 if negative
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	else
+	{
+		_putchar('0');
+		return (0);
+	}
+}
