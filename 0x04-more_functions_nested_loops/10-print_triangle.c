813770719bb2dcd3fd57196ba403152e387e8346
HeNoor <hechanenourelhouda@gmail.com>
2023-03-23 19:36:53 +0000

    10-print_triangle.c

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * print_triangle - Entry point
+ *
+ * Description: prints a triangle
+ *
+ * @size: checks input of function
+ *
+ * Return: Always 0 (Success)
+ */
+
+void print_triangle(int size)
+{
+	int i;
+	int j;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 1; i <= size; i++)
+		{
+			for (j = i; j < size; j++)
+			{
+				_putchar(' ');
+			}
+			for (j = 1; j <= i; j++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+}
