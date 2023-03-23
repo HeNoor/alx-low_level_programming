507072a36bcdddbf003a5caadc67689aa4b103a5
HeNoor <hechanenourelhouda@gmail.com>
2023-03-23 10:54:41 +0000

    4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - Entry point
+ *
+ * Description: prints all numbers except two and four
+ *
+ * Retur: Always 0 (Success)
+ */
+
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		if (i == 50 || i == 52)
+		{
+			continue;
+		}
+		else
+		{
+			_putchar(i);
+		}
+	}
+	_putchar('\n');
+}
