ddcc8a5549585cbe3115b9ff0a7e07be7894e190
HeNoor <hechanenourelhouda@gmail.com>
2023-03-23 11:40:05 +0000

    5-more_numbers.c

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * more_numbers - Entry point
+ *
+ * Description: prints ten times the numbers from zero to
+ *
+ * Return: Always 0 (Sucess)
+ */
+
+void more_numbers(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j <= 14; j++)
+		{
+			if (j > 9)
+			{
+				_putchar(49);
+			}
+			_putchar((j % 10) + 48);
+		}
+	}
+	_putchar('\n');
+}
