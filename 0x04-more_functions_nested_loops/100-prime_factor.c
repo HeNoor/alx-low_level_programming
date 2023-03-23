6015a07577b6d2e90cdbd089ef48cc0e3a9f8e52
HeNoor <hechanenourelhouda@gmail.com>
2023-03-23 21:36:38 +0000

    100-prime_factor.c

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: find the premier factor of a number
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	long int i;
+	long int maxfact;
+	long int n;
+
+	while (n % 2 == 0)
+	{
+		maxfact = 2;
+		n = n / 2;
+	}
+	for (i = 3; i <= n; i = i + 2)
+	{
+		while (n % i == 0)
+		{
+			maxfact = i;
+			n = n / i;
+		}
+	}
+	printf("%ld", maxfact);
+	return (0);
+}
