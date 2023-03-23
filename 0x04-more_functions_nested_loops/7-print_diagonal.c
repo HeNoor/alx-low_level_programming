7f930984d6424cce79c58db0c56a6c4929620eab
HeNoor <hechanenourelhouda@gmail.com>
2023-03-23 14:25:23 +0000

    7-print_diagonal.c

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -19,12 +19,12 @@ void print_diagonal(int n)
 	{
 		for (i = 1; i <= n; i++)
 		{
-			_putchar(92);
-			_putchar('\n');
 			for (j = 1; j <= i; j++)
 			{
 				_putchar(32);
 			}
+			_putchar(92);
+			_putchar('\n');
 		}
 	}
 	else
