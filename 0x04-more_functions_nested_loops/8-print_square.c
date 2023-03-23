bc6a6211e0e7c1d9a1211c2c436dc273f691b6fa
HeNoor <hechanenourelhouda@gmail.com>
2023-03-23 14:53:28 +0000

    8-print_square.c

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_square - Entry point
+ *
+ * Description: prints a square
+ *
+ * @size: checks input of function
+ *
+ * Return: Always 0 (Success)
+ */
+
+void print_square(int size)
+{
+	int i;
+	int j;
+
+	for (i = 1; i <= size; i++)
+	{
+		for (j = 1; j <= size; j++)
+		{
+			_putchar(35);
+		}
+		_putchar('\n');
+	}
+}
