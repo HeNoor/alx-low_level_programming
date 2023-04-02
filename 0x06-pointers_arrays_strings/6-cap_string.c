ad14762e40fbe059536cd286d5c5700890905e7d
HeNoor <hechanenourelhouda@gmail.com>
2023-04-02 22:32:31 +0000

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -12,20 +12,26 @@
 char *cap_string(char *str)
 {
 	int i = 0;
+	int j = 0;
+	char array[] = " \t\n,;.!?\"(){}";
 
-	while (str[i])
+	while (*(str + i))
 	{
-		while (!(str[i] >= 'a' && str[i] <= 'z'))
-			i++;
-
-		if (str[i - 1] == ' ' || str[i - 1] == '\t' ||
-				str[i - 1] == '\n' || str[i - 1] == ',' ||
-				str[i - 1] == ';' || str[i - 1] == '.' ||
-				str[i - 1] == '!' || str[i - 1] == '?' ||
-				str[i - 1] == '"' || str[i - 1] == '(' ||
-				str[i - 1] == ')' || str[i - 1] == '{' ||
-				str[i - 1] == '}' || i == 0)
-			str[i] -= 32;
+		if (*(str + i) <= 'z' && *(str + i) >= 'a')
+		{
+			if (i == 0)
+			{
+				str[i] = str[i] - 32;
+			}
+			else
+			{
+				for (j = 0; j != '\0'; j++)
+				{
+					if (str[i - 1] == array[j])
+						str[i] = str[i] - 32;
+				}
+			}
+		}
 		i++;
 	}
 	return (str);
