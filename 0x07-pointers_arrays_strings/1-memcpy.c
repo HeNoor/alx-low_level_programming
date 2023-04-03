f1ad9eeb2544beb31e05f7ba0a16c9bf9bc3a414
HeNoor <hechanenourelhouda@gmail.com>
2023-04-03 13:20:55 +0000

    1-memcpy.c

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * @dest: char input
+ * @src: char input
+ * @n: unsigned int input
+ *
+ * Return: returns a pointer to dest
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	int i;
+
+	for (i = 0; n > 0; n--, i++)
+	{
+		*(dest + i) = *(src + i);
+	}
+	return (dest);
+}
