72781c4346ccf79842b2745363f6377dbeabdea0
HeNoor <hechanenourelhouda@gmail.com>
2023-04-21 00:32:36 +0000

    0-sum_them_all.c

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+
+/**
+ * sum_them_all - add all numbers
+ * @n: n unsigned int typed numbers
+ * Return: sum
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list args;
+	unsigned int i;
+	unsigned int sum = 0;
+
+	va_start(args, n);
+
+	for (i = 0; i < n; i++)
+	{
+		sum = sum + va_arg(args, int);
+	}
+
+	va_end(args);
+
+	return (sum);
+}
