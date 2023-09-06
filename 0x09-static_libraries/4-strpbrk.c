a8de6dec61b0a232cc84825ed22e0684eb9e1ea2
HeNoor <hechanenourelhouda@gmail.com>
2023-09-06 15:52:01 +0000

    libmy.a

--- 0x09-static_libraries/4-strpbrk.c
+++ 0x09-static_libraries/4-strpbrk.c
@@ -1,1 +1,38 @@
+#include "main.h"
 
+/**
+ * _strpbrk - Entry point
+ *
+ * @s: char input
+ * @accept: char input second
+ *
+ * Return: Always 0 (Success)
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int i = 0;
+	int j = 0;
+
+	if (*(s + i) == '\0' || *(accept + j) == '\0')
+	{
+		return (0);
+	}
+	while (*(s + i) != '\0')
+	{
+		j = 0;
+		while (*(accept + j))
+		{
+			if (*(s + i) == *(accept + j))
+			{
+				return (&(*(s + i)));
+			}
+			else
+			{
+				j++;
+				continue;
+			}
+		}
+		i++;
+	}
+	return (0);
+}
