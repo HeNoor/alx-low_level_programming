1912d377e8f982eea824111d64bdbadbec0775a4
HeNoor <hechanenourelhouda@gmail.com>
2023-09-07 22:16:39 +0000

    0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
-/*
+/**
  * main - prints its name followed by a new line
  * @argc: int input
  * @argv: char input
  * Return: Always 0 (Success)
  */
 
-int main(int argc, char *argv[])
+int main(int argc, char const *argv[])
 {
 	(void)argc;
 	printf("%s\n", argv[0]);
